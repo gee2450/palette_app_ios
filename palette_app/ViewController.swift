063c5dafdefc63f05915397d42fbeafee5ce66c0
Minji Yun <gee2450@kookmin.ac.kr>
2023-03-10 20:48:45 +0900

    Setting AppCode .

---
+++ palette_app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  palette_app
+//
+//  Created by 윤민지 on 2023/03/10.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
