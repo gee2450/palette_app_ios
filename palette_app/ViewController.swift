730e2feb44d1d3a15cbdb1abe39ee9e36b4b9c99
Minji Yun <gee2450@kookmin.ac.kr>
2023-03-15 02:26:31 +0900

    Practice screen transition .

--- palette_app/ViewController.swift
+++ palette_app/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    let instance : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
 
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
     }
 
+    @IBAction
+    func clickBtn1(_ sender: Any) {
+        print("a")
+    }
 
+    @IBAction
+    func clickBtn2(_ sender: Any) {
+        print("b")
 
+        let a = instance.instantiateViewController(withIdentifier: "mainView")
+        self.show(a, sender: self)
+    }
 }
